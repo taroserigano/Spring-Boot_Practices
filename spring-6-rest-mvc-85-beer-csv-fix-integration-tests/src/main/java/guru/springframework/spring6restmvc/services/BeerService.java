406f7565ed44cd20cf95ca2e14914f0d5eae7409
Taro Serigano <taroserigano@gmail.com>
2023-05-09 18:08:41 -0400

    Add files via upload

---
+++ spring-6-rest-mvc-85-beer-csv-fix-integration-tests/src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.BeerDTO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public interface BeerService {
+
+    List<BeerDTO> listBeers();
+
+    Optional<BeerDTO> getBeerById(UUID id);
+
+    BeerDTO saveNewBeer(BeerDTO beer);
+
+    Optional<BeerDTO> updateBeerById(UUID beerId, BeerDTO beer);
+
+    Boolean deleteById(UUID beerId);
+
+    Optional<BeerDTO> patchBeerById(UUID beerId, BeerDTO beer);
+}
